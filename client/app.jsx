861576cb8e47f464ce1f322c9d241a93fdb0335c
Neil Murphy <wnmurphy@gmail.com>
2016-03-11 14:24:29 -0800

    Setting up routing for react.

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,35 @@
 /** @jsx React.DOM */
+var Router = window.ReactRouter.Router;
+var Route = window.ReactRouter.Route;
+var Link = window.ReactRouter.Link;
 
 var AppView = React.createClass({
   render: function() {
     console.log("Rendering");
     return (
-      <div>Hello New World</div>
+      <div>
+        AppView
+        <div><Link to="/spot">LinktoSpotView</Link></div>
+      </div>
     );
   }
 });
 
 ReactDOM.render(<AppView/>, document.getElementById('app-container'));
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={MapView} />
+    <Route path="/spot" component={SpotView} > 
+      <Route path="/spot/:spotId" component={Spot} />
+    </Route>
+    <Route path="/create" component={CreateASpotView} />
+    <Route path="/search" component={SearchView} />
+    <Route path="/feed" component={UserFeedView} />
+    <Route path="/signup" component={SignupView} />
+    <Route path="/login" component={LoginView} />
+    <Route path="/profile" component={ProfileView} >
+      <Route path="/profile/:profileId" component={Profile}/>
+    </Route>
+  </Router>
+), document.getElementById('app-container'));
