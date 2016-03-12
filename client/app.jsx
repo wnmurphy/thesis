102349eaf2cbc2aa88c3f8d3523c6e3b80185fda
Neil Murphy <wnmurphy@gmail.com>
2016-03-11 16:23:11 -0800

    Front-end routing complete. Added NavBar component.

--- client/app.jsx
+++ client/app.jsx
@@ -3,24 +3,6 @@ var Router = window.ReactRouter.Router;
 var Route = window.ReactRouter.Route;
 var Link = window.ReactRouter.Link;
 
-var AppView = React.createClass({
-  render: function() {
-    console.log("Rendering");
-
-    return (
-      <div>
-        NavBar
-        <div>
-          <Link to="/map">Map</Link>
-          <Link to="/search">Search</Link>
-          <Link to="/feed">Feed</Link>
-          <Link to="/profile">Profile</Link>
-        </div>
-      </div>
-    );
-  }
-});
-
 var routes = (
   <Router>
     <Route path="/" component={MapView} />
