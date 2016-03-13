6e64a65b98d4caca55eafee0eeafd23e6158f7b4
Calvin Le <invisiblebeard@gmail.com>
2016-03-12 17:11:56 -0800

    Fleshed out MapView component

--- client/views/MapView.jsx
+++ client/views/MapView.jsx
@@ -1,8 +1,29 @@
 /** @jsx React.DOM */
 
 var MapView = React.createClass({
+  
+  getInitialState: function () {
+    return {
+          
+    };
+  },
+
+  componentWillMount: function () {
+    $.ajax({
+      method: 'GET',
+      url: '/api/map',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({spots: data});
+        console.log("SUCCESS: ", data);
+      },
+      error: function(error) {
+        console.log("ERROR: ", error);
+      }
+    })
+  },
+
   render: function() {
-    console.log("Rendering MapView");
     return (
       <div>MapView</div>
     );
