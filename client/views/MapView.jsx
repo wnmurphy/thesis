00652176b4300f6156d2ea2fefdfa0c0f933a4ca
Jisoo Yoon <jyoon006@gmail.com>
2016-03-12 22:24:46 -0500

    (feat) add google map to map view

--- client/views/MapView.jsx
+++ client/views/MapView.jsx
@@ -9,6 +9,7 @@ var MapView = React.createClass({
   },
 
   componentWillMount: function () {
+    this.getLocation();
     var context = this;
     $.ajax({
       method: 'GET',
@@ -23,7 +24,40 @@ var MapView = React.createClass({
       }
     })
   },
-
+  componentDidUpdate: function () {
+    this.initMap();
+    this.render();
+  },
+  getLocation: function () {
+    var currentLocation = {};
+    var context = this;
+    // The following call is asynchronous and takes a few seconds...
+    // This should also probably be eventually moved to MapView, as
+    // location will be needed there first to render the map.
+    navigator.geolocation.getCurrentPosition(function(position){
+      currentLocation.latitude = position.coords.latitude;
+      currentLocation.longitude = position.coords.longitude;
+      context.setState({location: currentLocation});
+      console.log('context state', context.state.location);
+    }, function(error){
+      console.log(error);
+    });
+  },
+  initMap: function() {
+    // var mapProp = {
+    //   center:new google.maps.LatLng(51.508742,-0.120850),
+    //   zoom:5,
+    //   mapTypeId:google.maps.MapTypeId.ROADMAP
+    // };
+    // var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var context = this;
+    console.log('this state in init map', this.state);
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: context.state.location.latitude, lng: context.state.location.longitude},
+      scrollwheel: false,
+      zoom: 8
+    });
+  },
   render: function() {
     var spots = this.state.spots.map(function(spot) {
       return (
@@ -36,7 +70,8 @@ var MapView = React.createClass({
     }, this)
     return (
       <div>Map View
-      <div>{spots}</div>
+        <div id="map"></div>  
+        <div>{spots}</div>
       </div>
 
     );
