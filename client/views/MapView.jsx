dbe47a6eb7632f27b93e2ebbb12960d9e594c787
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-03-13 13:17:35 -0700

    Adds loading screen and loads map when located

--- client/views/MapView.jsx
+++ client/views/MapView.jsx
@@ -7,11 +7,25 @@ var MapView = React.createClass({
       spots: [],
       selected: {}
     };
+    this.getLocation();
   },
 
   componentWillMount: function () {
     this.getLocation();
+
     var context = this;
+
+    var checkForCurrentLocation = function () {
+      if (context.state.location) {
+        context.initMap();
+      } else {
+        setTimeout(checkForCurrentLocation, 100);
+      }
+    }
+
+    // Added this time out to always show welcome-div
+    setTimeout(checkForCurrentLocation, 1000);
+
     $.ajax({
       method: 'GET',
       url: '/api/map',
@@ -19,18 +33,12 @@ var MapView = React.createClass({
       success: function(data) {
         context.setState({spots: data});
         console.log("SUCCESS: ", context.state.spots);
+        context.initSpots();
       },
       error: function(error) {
         console.log("ERROR: ", error);
       }
     })
-  },
-
-  componentDidMount: function () {
-    var context = this;
-    setTimeout(function() {
-      context.initMap();
-    }, 100);
   },
 
   getLocation: function () {
@@ -39,6 +47,7 @@ var MapView = React.createClass({
     navigator.geolocation.getCurrentPosition(function(position){
       currentLocation.latitude = position.coords.latitude;
       currentLocation.longitude = position.coords.longitude;
+      console.log(currentLocation);
       context.setState({location: currentLocation});
     }, function(error){
       console.log(error);
@@ -50,7 +59,10 @@ var MapView = React.createClass({
 
     var position = {lat: this.state.location.latitude, lng: this.state.location.longitude};
 
+    console.log('initMap position:', position);
     var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeControl: false,
+      streetViewControl: false,
       center: position,
       scrollwheel: true,
       zoom: 13
@@ -63,6 +75,12 @@ var MapView = React.createClass({
     });
 
     myMarker.setIcon('http://maps.google.com/mapfiles/arrow.png');
+
+  },
+
+  initSpots: function () {
+
+    console.log("initializing spot markers");
 
     for(var i = 0; i < this.state.spots.length - 1; i++) {
 
@@ -95,7 +113,15 @@ var MapView = React.createClass({
   render: function () {
     return (
       <div className="map-view-container">
-        <div id="map"></div>
+        <div id="map">
+          <div className="welcome-container">
+            <div>
+              <h1>Welcome to Happn!</h1>
+              <h2>Find the Haps!</h2>
+              <p>(your HapMap is loading now)</p>
+            </div>
+          </div>
+        </div>
         <div className="create-button-container">
           <a href="#/create" className="circle">
             <i className="material-icons">add</i>
