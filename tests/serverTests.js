b1198dc58810833cb1d4a9c8ad15a39b4265d174
Michelle <mtlassig@gmail.com>
2016-03-16 13:30:30 -0500

    (fix) fixes error with asynchoronous seeding of db for tests

--- tests/serverTests.js
+++ tests/serverTests.js
@@ -34,6 +34,7 @@ describe("Persistent Spot and User Server", function() {
     dbSchema.scan(params, function(err, data) {
       //if any spots were found with name of 'test1', delete each of them
       if (data.Count > 0) {
+        var results = data.Items;
         data.Items.forEach(function(spot) {
           var id = spot.spotId;
           params = {
@@ -44,22 +45,25 @@ describe("Persistent Spot and User Server", function() {
           };
           dbSchema.delete(params, function(err, data) {
             if (err) {console.error("Error deleting Johnny's spot ", err);}
-            //create a new spot "test1" for testing
-            params = {
-              TableName: 'Spots',
-              Item: {
-                spotId: 999999999999,
-                name: "test1", 
-                creator: 'Johnny', 
-                category: 'entertainment',
-                location: 'Arizona',
-                description: 'test createSpot',
-                start: '10'
-              }
-            };
-            dbSchema.put(params, function(err, data) {
-              if (err) {console.error("Error creating Johnny's spot", err);}
-            });
+            //if we're on the final delete create a new spot "test1" for testing
+            if (id === results[results.length - 1].spotId) {
+              params = {
+                TableName: 'Spots',
+                Item: {
+                  spotId: 999999999999,
+                  name: "test1", 
+                  creator: 'Johnny', 
+                  category: 'entertainment',
+                  location: 'Arizona',
+                  description: 'test createSpot',
+                  start: '10'
+                }
+              };
+              dbSchema.put(params, function(err, data) {
+                if (err) {console.error("Error creating Johnny's spot", err);}
+                seedUser();
+              });
+            }
           });
         });
       } else {
@@ -78,6 +82,7 @@ describe("Persistent Spot and User Server", function() {
         };
         dbSchema.put(params, function(err, data) {
           if (err) {console.error("Error creating Johnny's spot", err);}
+          seedUser();
         });
       }
     });
@@ -95,6 +100,7 @@ describe("Persistent Spot and User Server", function() {
       };
       dbSchema.scan(params, function(err, data) {
         if (data.Count > 0) {
+          var results = data.Items;
           data.Items.forEach(function(user) {
             var id = user.userId;
             params = {
@@ -105,20 +111,22 @@ describe("Persistent Spot and User Server", function() {
             };
             dbSchema.delete(params, function(err, data) {
               if (err) {console.error("Error deleting test users ", err);}
-              //create a new user "Johnny for testing"
-              params = {
-                TableName: 'Users',
-                Item: {
-                  userId: 999999999999,
-                  username: 'Johnny',
-                  password: 'password',
-                  email: 'test@gmail.com'
-                }
-              };
-              dbSchema.put(params, function(err, data) {
-                if (err) {console.error("Error creating Johnny", err);}
-                done();
-              });
+              //if we're on the final delete, create a new user "Johnny for testing"
+              if (id === results[results.length - 1].userId) {
+                params = {
+                  TableName: 'Users',
+                  Item: {
+                    userId: 999999999999,
+                    username: 'Johnny',
+                    password: 'password',
+                    email: 'test@gmail.com'
+                  }
+                };
+                dbSchema.put(params, function(err, data) {
+                  if (err) {console.error("Error creating Johnny", err);}
+                  done();
+                });
+              }
             });
           });
         } else {
@@ -248,7 +256,6 @@ describe("Persistent Spot and User Server", function() {
         if (err) {
           console.error(err);
         }
-        console.log("All spots: ", spots.Items);
       });
       params = {
         TableName: 'Spots',
@@ -261,15 +268,12 @@ describe("Persistent Spot and User Server", function() {
         }
       };
       dbSchema.scan(params, function(err, spots) {
-        console.log("spots: ", spots.Items);
         if (err) {
           console.error("error creating Johnny's spot", err);
           done();
         }
         var spot = spots.Items[0];
         helpers.getSpot(spot.spotId, function(found){
-                  console.log("in here");
-
           expect(found.spotId).to.equal(spot.spotId);
           expect(found.name).to.equal('test1');
           expect(found.category).to.equal('entertainment');
@@ -335,6 +339,39 @@ describe("Server routes", function() {
       }
       expect(res.statusCode).to.equal(200);
       expect(body).to.be.not.empty;
+      done();
+    });
+  });
+  it('should return a 200 when gettting spots', function(done) {
+    request({
+      method: "GET",
+      uri: "http://localhost:8080/api/map"
+    }, function (err, res, body){
+      if (err) {
+        console.error("Error getting spots ", err);
+        done();
+      }
+      expect(res.statusCode).to.equal(200);
+      expect(body.length).to.be.above(0);
+      done();
+    });
+  });
+  it('should return a 200 when signing up a user', function(done) {
+    request({
+      method: "POST",
+      uri: "http://localhost:8080/api/signup",
+      json: {
+        username: "test",
+        password:  "test",
+        email:  "test@gmail.com"
+      }
+    }, function(err, res, body) {
+      if (err) {
+        console.error("Error signing up user ", err);
+        done();
+      }
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.be.empty;
       done();
     });
   });
