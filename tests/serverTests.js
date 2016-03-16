1d42b852ef15aae48ee345384023d3b5429a291b
Michelle <mtlassig@gmail.com>
2016-03-15 22:37:11 -0500

    (chore) refactors to seed database with a user and spot to test with

--- tests/serverTests.js
+++ tests/serverTests.js
@@ -20,7 +20,7 @@ describe("Persistent Spot and User Server", function() {
     });
 
     
-    //delete previous testing spot
+    //delete previous testing spot(s)
     var params = {
       TableName: 'Spots',
       FilterExpression: "#name = (:name)",
@@ -42,31 +42,60 @@ describe("Persistent Spot and User Server", function() {
         };
         dbSchema.delete(params, function(err, data) {
           if (err) {console.error("Error deleting Johnny's spot ", err);}
+          //create a new spot "test1" for testing
+          params = {
+            TableName: 'Spots',
+            Item: {
+              name: "test1", 
+              creator: 'Johnny', 
+              category: 'entertainment',
+              location: 'Arizona',
+              description: 'test createSpot',
+              start: '10'
+            }
+          };
+          dbSchema.put(params, function(err, data) {
+            if (err) {console.error("Error creating Johnny's spot ", err);}
+          });
         });
       });
     });
     //delete previously signed up test user
     params = {
       TableName: 'Users',
-      FilterExpression: "#name = (:name)",
+      FilterExpression: "#email = (:email)",
       ExpressionAttributeNames:{
-          "#name": "username"
+          "#email": "email"
       },
       ExpressionAttributeValues: {
-          ":name": 'Johnny'
+          ":email": 'test@gmail.com'
       }
     };
     dbSchema.scan(params, function(err, data) {
       if (data.Count > 0) {
-        var id = data.Items[0].userId;
-        params = {
-          TableName: 'Users',
-          Key: {
-            userId: id
-          }
-        };
-        dbSchema.delete(params, function(err, data) {
-          if (err) {console.error("Error deleting Johnny ", err);}
+        data.Items.forEach(function(user) {
+          var id = user.userId;
+          params = {
+            TableName: 'Users',
+            Key: {
+              userId: id
+            }
+          };
+          dbSchema.delete(params, function(err, data) {
+            if (err) {console.error("Error deleting test users ", err);}
+            //create a new user "Johnny for testing"
+            params = {
+              TableName: 'Users',
+              Item: {
+                username: 'Johnny',
+                password: 'password',
+                email: 'test@gmail.com'
+              }
+            };
+            dbSchema.put(params, function(err, data) {
+              if (err) {console.error("Error creating Johnny ", err);}
+            });
+          });
         });
       }
     });
@@ -77,20 +106,20 @@ describe("Persistent Spot and User Server", function() {
     it("should create spots and be able to search for spots", function(done){
       helpers.createSpot({
         name: "test1", 
-        creator: 'Johnny', 
+        creator: 'Bob', 
         category: 'entertainment',
-        location: 'Arizona',
+        location: 'Minnesota',
         description: 'test createSpot',
         start: '10'
       }, function(){
         params = {
           TableName: "Spots",
-          FilterExpression: "#name = (:name)",
+          FilterExpression: "#location = (:location)",
           ExpressionAttributeNames:{
-              "#name": "name"
+              "#location": "location"
           },
           ExpressionAttributeValues: {
-              ":name": 'test1'
+              ":location": 'Minnesota'
           }
         };
         dbSchema.scan(params, function(err, data){
@@ -101,11 +130,11 @@ describe("Persistent Spot and User Server", function() {
           else {
             var spot = data.Items[0];
             expect(spot.name).to.equal('test1');
-            expect(spot.creator).to.equal('Johnny');
-            expect(spot.location).to.equal('Arizona');
-            helpers.search('test', function(results){
+            expect(spot.creator).to.equal('Bob');
+            expect(spot.location).to.equal('Minnesota');
+            helpers.search('Bo', function(results){
               expect(results.length).to.be.above(0);
-              expect(results[0].name).to.contain('test');
+              expect(results[0].creator).to.contain('Bo');
               done();
             }, function(err){
               console.error("Failed to search for spot", err);
@@ -120,131 +149,87 @@ describe("Persistent Spot and User Server", function() {
     });
     it('should sign users up and be able to search for users', function(done) {
       helpers.signup({
-        username: 'Johnny',
+        username: 'Bob',
         password: 'password',
         email: 'test@gmail.com'
       }, 
       function(user){
-        helpers.search('John', function(users) {
-          //search works here
+        helpers.search('Bob', function(users) {
           expect(users.length).to.be.above(0);
-          expect(users[0].username).to.contain('John');
+          expect(users[0].username).to.contain('Bob');
           done();
         }, 
         function(err) {
-          console.error('Failed in search for John', err);
+          console.error('Failed in search for Bob', err);
           done();
         });
       }, function(err){
-        console.error("John's Error: ", err);
+        console.error("Bob's Error: ", err);
         done();
       });
     });
     it('should be able to get spots', function(done) {
-      helpers.createSpot({
-          name: "test1", 
-          creator: 'Johnny', 
-          category: 'entertainment',
-          location: 'Arizona',
-          description: 'test createSpot',
-          start: '10'
-        }, function(data){
-          helpers.getSpots(null, function(array){
-            expect(array).to.be.an('array');
-            expect(array.length).to.be.above(0);
-            done();
-          }, function(err){
-            console.error('Error getting spots', err);
-            done();
-          });
-        }, function(err){
-          console.error("Error populating spot", err);
-          done();
-        });
+      helpers.getSpots(null, function(array){
+        expect(array).to.be.an('array');
+        expect(array.length).to.be.above(0);
+        done();
+      }, function(err){
+        console.error('Error getting spots', err);
+        done();
+      });
     });
     it('should sign in a user', function(done) {
-      helpers.signup({
+      helpers.signin({
         username: 'Johnny',
         password: 'password',
-        email: 'test@gmail.com'
-      }, function(data){
-        helpers.signin({
-          username: 'Johnny',
-          password: 'password',
-        }, function(user){
-          //expects
-          expect(user.Items[0].password).to.equal('password');
-          expect(user.Items[0].username).to.equal('Johnny');
-          done();
-        }, function(err){
-          console.error("Error signing in Johnny", err);
-          done();
-        });
-      }, function(err) {
-        console.error("Error signing up Johnny", err);
+      }, function(user){
+        //expects
+        expect(user.Items[0].password).to.equal('password');
+        expect(user.Items[0].username).to.equal('Johnny');
+        done();
+      }, function(err){
+        console.error("Error signing in Johnny", err);
         done();
       });
     });
     it('should get user\' profile', function(done) {
-      helpers.signup({
-        username: 'Johnny',
-        password: 'password',
-        email: 'test@gmail.com'
-      }, function(data){
-        helpers.getProfile('Johnny', function(profile){
-          expect(profile.userId).to.exist();
-          expect(profile.username).to.equal('Johnny');
-          expect(profile.email).to.equal('test@gmail.com');
-          done();
-        }, function(err){
-          console.error("Error getting Johnny\'s profile", err);
-          done();
-        });
-      }, function(err) {
-        console.error("Error signing up Johnny", err);
+      helpers.getProfile('Johnny', function(profile){
+        expect(profile.userId).to.exist();
+        expect(profile.username).to.equal('Johnny');
+        expect(profile.email).to.equal('test@gmail.com');
+        done();
+      }, function(err){
+        console.error("Error getting Johnny\'s profile", err);
         done();
       });
     });
     it('should get a spot', function(done) {
-      helpers.createSpot({
-        name: "test1", 
-        creator: 'Johnny', 
-        category: 'entertainment',
-        location: 'Arizona',
-        description: 'test createSpot',
-        start: '10'
-      }, function(spot){
-        params = {
-          TableName: 'Spots',
-          FilterExpression: "#name = (:name)",
-          ExpressionAttributeNames:{
-              "#name": "name"
-          },
-          ExpressionAttributeValues: {
-              ":name": 'test1'
-          }
-        };
-        dbSchema.scan(params, function(err, spots) {
-          if (err) {
-            console.error("error creating Johnny's spot", err);
-            done();
-          } else {
-            var spot = spots.Items[0];
-            helpers.getSpot(spot.spotId, function(found){
-              expect(found.spotId).to.equal(spot.spotId);
-              expect(found.name).to.equal('test1');
-              expect(found.category).to.equal('entertainment');
-              done();
-            }, function(err) {
-              console.error("error getting Johnny's spot", err);
-              done();
-            });
-          }
-
+      params = {
+        TableName: 'Spots',
+        FilterExpression: "#name = (:name)",
+        ExpressionAttributeNames: {
+            "#name": "name"
+        },
+        ExpressionAttributeValues: {
+            ":name": 'test1'
+        }
+      };
+      dbSchema.scan(params, function(err, spots) {
+        console.log('spots: ', spots);
+        if (err) {
+          console.error("error creating Johnny's spot", err);
+          done();
+        }
+        var spot = spots.Items[0];
+        helpers.getSpot(spot.spotId, function(found){
+          expect(found.spotId).to.equal(spot.spotId);
+          expect(found.name).to.equal('test1');
+          expect(found.category).to.equal('entertainment');
+          done();
+        }, function(err) {
+          console.error("error getting Johnny's spot", err);
+          done();
         });
-      }, function(err) {
-        console.error("error creating Johnny's spot", err);
-        done();
       });
     });
     //need to write tests for helper function distanceBetween
@@ -274,7 +259,7 @@ describe("Server routes", function() {
         name: "test1", 
         creator: 'Johnny', 
         category: 'entertainment',
-        location: 'Arizona',
+        location: 'Montana',
         description: 'test createSpot',
         start: '10'
       }
@@ -289,34 +274,20 @@ describe("Server routes", function() {
     });
   });
   it('should return a 200 when searching', function(done) {
-    helpers.createSpot({
-      name: "test1", 
-      creator: 'Johnny', 
-      category: 'entertainment',
-      location: 'Arizona',
-      description: 'test createSpot',
-      start: '10'
-    }, function(data) {
-      request({
-        method: "POST",
-        uri: "http://localhost:8080/api/search",
-        json: {
-          search: "test"
-        }
-      }, function(err, res, body) {
-        if (err) {
-          console.error("Error creating new spot ", err);
-          done();
-        }
-        expect(res.statusCode).to.equal(200);
-        expect(body).to.be.not.empty;
-        done();
-      });
-    }, function(err) {
+    request({
+      method: "POST",
+      uri: "http://localhost:8080/api/search",
+      json: {
+        search: "test"
+      }
+    }, function(err, res, body) {
       if (err) {
         console.error("Error creating new spot ", err);
         done();
       }
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.be.not.empty;
+      done();
     });
   });
 });
