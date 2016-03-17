d6b47295fdb653b9acbfc4b5a426f67b4b4564d1
Calvin Le <invisiblebeard@gmail.com>
2016-03-17 15:17:26 -0700

    (refactor) refactored time controller to return time with hours and minutes

--- client/controllers/TimeController.js
+++ client/controllers/TimeController.js
@@ -1,25 +1,7 @@
 var getTime = function() {
-  var time;
+  var time = {};
   var date = new Date();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var meridian;
-
-  if (hours < 12) {
-    meridian = 'am';
-  } else {
-    meridian = 'pm';
-  }
-
-  if (hours > 12) {
-    hours -= 12;
-  } else if (hours === 0) {
-    hours = 12;
-  }
-
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
-
-  time = hours + ':' + minutes + meridian;
+  time.hours = date.getHours();
+  time.minutes = date.getMinutes();
+  return time;
 };
