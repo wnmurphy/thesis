cee9af23eaf5d832f350c12dca515789a372e05b
Jisoo Yoon <jyoon006@gmail.com>
2016-03-19 16:37:19 -0400

    (refactor) mino change to setState of globalState before callback in getLocation controller

--- client/controllers/LocationController.js
+++ client/controllers/LocationController.js
@@ -4,9 +4,10 @@ var getLocation = function (callback, context) {
     navigator.geolocation.getCurrentPosition(function(position){
       currentLocation.latitude = position.coords.latitude;
       currentLocation.longitude = position.coords.longitude;
-      callback(currentLocation);
       globalState.location = currentLocation;
       context.setState({location: currentLocation});
+      callback(currentLocation);
+      
     }, function(error){
       console.log(error);
   });
