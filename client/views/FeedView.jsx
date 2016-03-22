2f734e049cdd50f73b7b914539abfd44bc469971
Michelle <mtlassig@gmail.com>
2016-03-22 15:36:20 -0500

    (feat) rendering saved spots and followed users' spots on page

--- client/views/FeedView.jsx
+++ client/views/FeedView.jsx
@@ -27,18 +27,38 @@ var FeedView = React.createClass({
 
   render: function() {
     console.log("Rendering FeedView");
+    console.log("Saved spots: ", this.state.savedSpots);
+    console.log("Followed Users' Spots: ", this.state.followedUsersSpots);
+    var savedSpots = this.state.savedSpots.map(function (spot) {
+      return (
+        <div className ='saved-spot'>
+          <div className = 'saved-spot-name'>{spot.name}</div>
+          <div className = 'saved-spot-creator'>{spot.creator}</div>
+          <div className = 'saved-spot-start'>{spot.start}</div>
+        </div>
+      );
+    }, this);
+    var followedUsersSpots = this.state.followedUsersSpots.map(function(spot) {
+      return (
+        <div className = 'followed-user-spot'>
+          <div className = 'followed-user-spot-name'>{spot.name}</div>
+          <div className = 'followed-user-spot-creator'>{spot.creator}</div>
+          <div className = 'followed-user-spot-start'>{spot.start}</div>
+        </div>
+      );
+    });
+
     return (
       <div>
         <LoginRequired />
-        <div className='saved-spot-container'>Saved Spots</div>
-        <div className='followed-userSpot-container'>Feed</div>
+        <div >Saved Spots
+          <div className='saved-spot-container'>{savedSpots}</div>
+        </div>
+        <div>Feed
+          <div className='followed-user-spot-container'>{followedUsersSpots}</div>
+        </div>
       </div>
     );
   }
 });
 
-// <div className ='saved-spot'>
-//   <div className = 'saved-spot-name'>{spot.name}</div>
-//   <div className = 'saved-spot-creator'>{spot.creator}</div>
-//   <div className = 'saved-spot-start'>{spot.start}</div>
-// </div>
