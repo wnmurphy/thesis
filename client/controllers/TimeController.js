8eebb048d2aee534ffd7fc7cfcff9af284a3ef3b
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-03-23 19:22:12 -0700

    (refactor) time controller functions

--- client/controllers/TimeController.js
+++ client/controllers/TimeController.js
@@ -10,3 +10,21 @@ var getTime = function() {
   }
   return hours + minutes;
 };
+
+var timeController = {
+
+  // takes a time string as input and returns a date object for that time today
+  timeToDate: function (time) {
+    var now = new Date();
+    var month = now.getMonth() + 1,
+        day = now.getDate(),
+        year = now.getFullYear();
+    if (now.getHours() > time.split(':')[0]) day++;
+    var input = [month, day, year, time].join(" ");
+    return new Date(Date.parse(input));
+  },
+
+  timeToMS: function (time) {
+    return timeController.timeToDate(time).getTime();
+  }
+}
