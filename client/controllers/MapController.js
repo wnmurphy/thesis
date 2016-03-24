1dcf4bbd71630cb2272c1a57ef4947d38983f373
Calvin Le <invisiblebeard@gmail.com>
2016-03-24 15:14:13 -0700

    (refactor) code clean up and refactored map controller

--- client/controllers/MapController.js
+++ client/controllers/MapController.js
@@ -1,245 +1,188 @@
+// Array containing style configuration for Google Maps.
+var style = [
+  {
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#6195a0" }
+    ]
+  },
+
+  {
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+
+  {
+    "featureType": "landscape",
+    "elementType": "geometry",
+    "stylers": [
+      { "lightness": "0" },
+      { "saturation": "0" },
+      { "color": "#f5f5f2" },
+      { "gamma": "1" }
+    ]
+  },
+
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "all",
+    "stylers": [
+      { "lightness": "-3" },
+      { "gamma": "1.00" }
+    ]
+  },
+
+  {
+    "featureType": "landscape.natural.terrain",
+    "elementType": "all",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+
+  {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#bae5ce" },
+      { "visibility": "on" }
+    ]
+  },
+
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      { "saturation": -100 },
+      { "lightness": 45 },
+      { "visibility": "simplified" }
+    ]
+  },
+
+  {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [
+      { "visibility": "simplified" }
+    ]
+  },
+
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#fac9a9" },
+      { "visibility": "simplified" }
+    ]
+  },
+
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text",
+    "stylers": [
+      { "color": "#4e4e4e" }
+    ]
+  },
+
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#787878" }
+    ]
+  },
+
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },
+
+  {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [
+      { "visibility": "simplified" }
+    ]
+  },
+
+  {
+    "featureType": "transit.station.airport",
+    "elementType": "labels.icon",
+    "stylers": [
+      { "hue": "#0a00ff" },
+      { "saturation": "-77" },
+      { "gamma": "0.57" },
+      { "lightness": "0" }
+    ]
+  },
+
+  {
+    "featureType": "transit.station.rail",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#43321e" }
+    ]
+  },
+
+  {
+    "featureType": "transit.station.rail",
+    "elementType": "labels.icon",
+    "stylers": [
+      { "hue": "#ff6c00" },
+      { "lightness": "4" },
+      { "gamma": "0.75" },
+      { "saturation": "-68" }
+    ]
+  },
+
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      { "color": "#eaf6f8" },
+      { "visibility": "on" }
+    ]
+  },
+
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#c7eced" }
+    ]
+  },
+
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "lightness": "-49" },
+      { "saturation": "-53" },
+      { "gamma": "0.79" }
+    ]
+  }
+]
+
 // Create a Google Map element.
 // Takes current user location, component as context to render within, and callback.
 var initMap = function (location, context, callback) {
 
   var position = new google.maps.LatLng(location.latitude, location.longitude);
-
-  context.setState({position: position});
-
-  // Array containing style configuration for Google Maps.
-  var style = [
-    {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#6195a0"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.province",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": "0"
-            },
-            {
-                "saturation": "0"
-            },
-            {
-                "color": "#f5f5f2"
-            },
-            {
-                "gamma": "1"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "all",
-        "stylers": [
-            {
-                "lightness": "-3"
-            },
-            {
-                "gamma": "1.00"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.natural.terrain",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#bae5ce"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#fac9a9"
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "color": "#4e4e4e"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#787878"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.airport",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "hue": "#0a00ff"
-            },
-            {
-                "saturation": "-77"
-            },
-            {
-                "gamma": "0.57"
-            },
-            {
-                "lightness": "0"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.rail",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#43321e"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.rail",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "hue": "#ff6c00"
-            },
-            {
-                "lightness": "4"
-            },
-            {
-                "gamma": "0.75"
-            },
-            {
-                "saturation": "-68"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#eaf6f8"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#c7eced"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "lightness": "-49"
-            },
-            {
-                "saturation": "-53"
-            },
-            {
-                "gamma": "0.79"
-            }
-        ]
-    }
-]
 
   // Create a new map and append to #map element.
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -252,11 +195,9 @@ var initMap = function (location, context, callback) {
   });
 
   // Create a new style object for map, using the config array above.
-  var type = new google.maps.StyledMapType(style, {name: '/'});
-
-  map.mapTypes.set('/', type);
-
-  map.setMapTypeId('/');
+  var type = new google.maps.StyledMapType(style, {name: 'IRL'});
+  map.mapTypes.set('IRL', type);
+  map.setMapTypeId('IRL');
 
   // Extend Map prototype with ability to offset the map center.
   // Useful for styling map around other elements.
@@ -283,7 +224,6 @@ var initMap = function (location, context, callback) {
     context.setState({center: center});
     context.getSpots();
   })
-  context.setState({map: map});
 
   var icon = {
     url: '/img/map/you_test.png'
@@ -300,7 +240,11 @@ var initMap = function (location, context, callback) {
     }
   });
 
+  context.setState({position: position});
+  context.setState({map: map});
   context.setState({marker: marker});
 
   callback(map, position, marker);
 }
+
+
