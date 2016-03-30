37c778144fde121af9b0d55526bca9ad08788f94
Jisoo Yoon <jyoon006@gmail.com>
2016-03-30 13:54:23 -0400

    (style) change style attributes to camel cases to remove warnings from react

--- client/views/SpotView.jsx
+++ client/views/SpotView.jsx
@@ -129,7 +129,7 @@ var SpotView = React.createClass({
     var chatContainerClass = "chat-card-container";
 
     if (this.state.end) {
-      var end = <p style={{'font-style': 'italic', 'font-size': '14px'}}>{this.state.end}</p>
+      var end = <p style={{'font-style': 'italic', 'fontSize': '14px'}}>{this.state.end}</p>
     } else {
       var end = null;
     }
@@ -144,14 +144,14 @@ var SpotView = React.createClass({
           </div>
         </div>
         <div className='spot-view-container'>
-          <p style={{color: '#122931', 'background-color': '#fff', margin: '10px', 'text-align': 'center', 'font-size': '24'}}>{this.state.spot.name}</p>
-          <p style={{color: '#122931', 'margin-top': '2.5px', 'text-align': 'center'}}>{timeController.stringifyTime(this.state.spot, true)}</p>
-          <a href={this.state.creatorId}><div className='spot-view-profile-picture' style={{'background-image': 'url(' + this.state.spot.img + ')'}}></div></a>
-          <p style={{color: '#4A5053', margin: '1px', 'text-align': 'center'}}><small>Created by {this.state.spot.creator}</small></p>
-          <p style={{'padding-top': '7.5px', 'font-size': '1.5em', color: '#4A5053', margin: '1px', 'text-align': 'center'}}><i className={categories[this.state.spot.category]}></i></p>
-          <p style={{color: '#122931', 'margin-top': '15px', 'text-align': 'center'}}>About This Spot</p>
-          <div className='description-container' style={{display: 'block', width: '350px', 'max-width': '100%', margin: '0 auto'}}>
-            <p style={{color: '#8B9596', 'text-align': 'justify'}}>{this.state.spot.description}</p>
+          <p style={{color: '#122931', 'backgroundColor': '#EFF0F1', margin: '10px', 'textAlign': 'center', 'fontSize': '24'}}>{this.state.spot.name}</p>
+          <p style={{color: '#122931', 'marginTop': '2.5px', 'textAlign': 'center'}}>{timeController.stringifyTime(this.state.spot, true)}</p>
+          <div className='spot-view-profile-picture' style={{'backgroundImage': 'url(' + this.state.spot.img + ')'}}></div>
+          <p style={{color: '#4A5053', margin: '1px', 'textAlign': 'center'}}><small>Created by <a style={{color: '#4A5053'}} href={this.state.creatorId} className="spot-view-creatorid">{this.state.spot.creator}</a></small></p>
+          <p style={{'paddingTop': '7.5px', 'fontSize': '1.5em', color: '#4A5053', margin: '1px', 'textAlign': 'center'}}><i className={categories[this.state.spot.category]}></i></p>
+          <p style={{color: '#122931', 'marginTop': '15px', 'textAlign': 'center'}}>About This Spot</p>
+          <div className='description-container' style={{display: 'block', width: '350px', 'maxWidth': '100%', margin: '0 auto'}}>
+            <p style={{color: '#8B9596', 'textAlign': 'justify'}}>{this.state.spot.description}</p>
           </div>
             <div className='button-container' style={{display: 'block', width: '300px', 'max-width': '100%', margin: '0 auto'}}>
               <DirectionsLink location={this.state.spot.location} />
