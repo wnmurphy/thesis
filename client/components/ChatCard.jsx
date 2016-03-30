9d35ec19c3b3176ab62486082540bcef26ddfe6f
Calvin Le <invisiblebeard@gmail.com>
2016-03-30 14:03:19 -0700

    (style) fixed css for self-chat bubble

--- client/components/ChatCard.jsx
+++ client/components/ChatCard.jsx
@@ -53,6 +53,11 @@ var Chat = React.createClass({
       } else {
         var bubble = "chat-bubble clear";
       }
+      var self = <div className={bubble}>
+                    <div className="message-text">{message.text}</div>
+                    <div className="message-footer">{message.username} @{timeController.timestampToTime(message.timeStamp)}</div>
+                  </div>;
+
       return (
         <div className={bubble}>
           <div className="message-text">{message.text}</div>
@@ -63,19 +68,13 @@ var Chat = React.createClass({
 
     return (
       <div className="chat-card">
-
         <div className="chat-container">
-
           <div className="chat">
           {messages}
           </div>
-
         </div>
-
         <div className="chat-form-container">
-
           <form className="chat-form" onSubmit={this.handleSubmit}>
-
             <input type="text" className="chat-input" defaultValue={this.state.message} placeholder="speak yo mind, russell" name="message" onChange={this.handleChange} autoComplete='off'/>
             <div className="button small right" onClick={this.handleSubmit}>send</div>
           </form>
